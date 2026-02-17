07a8b340cda872a03e9a5151d18d6aba4be22e27
Aditya Tangkas mardatila <aditya.mardatila@gmail.com>
2026-02-17 17:10:44 +0700

    Delete Belajar Python/Variabel dan Assignment.py

--- Belajar Python/Variabel dan Assignment.py
+++
@@ -1,9 +0,0 @@
-"""
-TODO:
-- Buatlah sebuah variabel bernama greeting.
-- Assign teks "Saya mulai belajar Python!" pada variabel tersebut. 
-"""
-
-# TODO: Silakan buat kode Anda di bawah ini.
-greeting = "Saya mulai belajar Python!"
-print(greeting)
